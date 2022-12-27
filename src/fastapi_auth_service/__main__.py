c353b3a3c1d2d04008c8fd128a1df7cbe9f75cb8
Oleg Korsak <kamikaze.is.waiting.you@gmail.com>
2022-12-27 22:21:16 +0200

    code fixes and improvements

--- src/fastapi_auth_service/__main__.py
+++ src/fastapi_auth_service/__main__.py
@@ -2,7 +2,5 @@ import uvicorn
 
 from fastapi_auth_service.api.http import app
 from fastapi_auth_service.conf import settings
-from fastapi_auth_service.extmod import c_fib
 
-print(f'{c_fib(10)=}')
 uvicorn.run(app, host=settings.service_addr, port=settings.service_port)
