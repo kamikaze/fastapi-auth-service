c8238e9c6212128ebbf8eaa61032f72a610376b8
Oleg Korsak <kamikaze.is.waiting.you@gmail.com>
2022-12-28 19:07:38 +0200

    fixed imports

--- src/fastapi_auth_service/core.py
+++ src/fastapi_auth_service/core.py
@@ -1,12 +1,12 @@
 import gettext
 import logging
 from pathlib import Path
-from typing import Mapping, Optional, Sequence
+from typing import Mapping, Optional
 
 import sqlalchemy as sa
-from databases.backends.postgres import Record
 from fastapi_pagination.bases import AbstractPage
 from fastapi_pagination.ext.async_sqlalchemy import paginate
+from sqlalchemy.engine import Result
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from fastapi_auth_service.db.models import UserGroup, User
@@ -17,21 +17,21 @@ _ = t.gettext
 
 
 async def get_users(session: AsyncSession, search: Optional[Mapping[str, str]] = None,
-                    order_by: Optional[str] = None) -> AbstractPage[Record]:
+                    order_by: Optional[str] = None) -> AbstractPage[Result]:
     query = sa.select([User])
     result = await paginate(session, query)
 
     return result
 
 
-async def get_user(session: AsyncSession, user_id: str) -> Record:
+async def get_user(session: AsyncSession, user_id: str) -> Result:
     query = sa.select([User]).where(User.id == user_id)
 
     return await session.execute(query)
 
 
 async def get_user_groups(session: AsyncSession, search: Optional[Mapping[str, str]] = None,
-                          order_by: Optional[str] = None) -> AbstractPage[Record]:
+                          order_by: Optional[str] = None) -> AbstractPage[Result]:
     query = sa.select([UserGroup]).order_by(UserGroup.name)
     result = await paginate(session, query)
 
